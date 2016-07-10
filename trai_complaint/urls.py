d180d69d06ac4f72178d7c7628c213f5bbd3da8d
kewal07 <kewal07@gmail.com>
2016-07-10 08:50:36 +0530

    sign in up

--- trai_complaint/urls.py
+++ trai_complaint/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from trai_complaint import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name="index"),
     url(r'^dashboard/', views.DashboardView.as_view(),name="dashboard"),
-    url(r'^add-complaint/', views.AddComplaintView.as_view(),name="add-complaint"),
-    url(r'^create-complaint/', views.CreateComplaintView.as_view(),name="create-complaint"),
+    url(r'^add-complaint/', login_required(views.AddComplaintView.as_view()),name="add-complaint"),
+    url(r'^create-complaint/', login_required(views.CreateComplaintView.as_view()),name="create-complaint"),
     url(r'^cust_details/', views.AjaxQueriesView.as_view(),name="cust_details"),
     url(r'^signup/', views.SignUpFormView.as_view(),name="signup"),
     url(r'^signin/', views.SignInFormView.as_view(),name="signin"),
