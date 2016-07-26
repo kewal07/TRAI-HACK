9e598b7bb48bea3e342a74c17a2d350a3e1a68f6
Ubuntu <ubuntu@ip-172-31-40-199.us-west-2.compute.internal>
2016-07-26 06:59:09 +0000

    server changes

--- trai_complaint/views.py
+++ trai_complaint/views.py
@@ -13,6 +13,8 @@ from django.core.mail import send_mail
 
 circles = ['Andhra Pradesh', 'Assam', 'Bihar & Jharkhand', 'Chennai', 'Delhi', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu & Kashmir', 'Karnataka', 'Kerala', 'Kolkata', 'Madhya Pradesh & Chhattisgarh', 'Maharashtra', 'Mumbai', 'North East', 'Orissa', 'Punjab', 'Rajasthan', 'Tamilnadu', 'UP-East', 'UP-West & Uttarakhand', 'West Bengal']
 
+session = str(randint(100,1000))
+
 class SignUpFormView(generic.ListView):
 	template_name = 'trai_complaint/signup.html'
 	def get_queryset(self):
@@ -44,11 +46,11 @@ class IndexView(generic.ListView):
 			userMsg = request.POST.get('message')
 			actions = {
 			    'getBalance': getBalance,
-				'gettransactions':gettransactions,
+			    'gettransactions':gettransactions,
 			}
 			client = Wit(access_token='TDBAKQ4EH4VKWJ4WXU3XD5RCICMUBHUH', actions=actions)
-			print(userMsg)
-			resp = client.converse('my-user-session-42', userMsg, {})
+			print(userMsg, session)
+			resp = client.converse('MY_SESSION_XYVSYGsdjghfjsdhgjh', userMsg, {})
 			print(resp)
 			if 'msg' in resp:
 				success['msg'] = resp.get('msg')
@@ -322,6 +324,8 @@ def excel_view(request):
 def getBalance(request, response):
 	return "20"
 
+def send(request, response):
+	return "sending..."
 def gettransactions():
 	return 'Subscribed to caller tune'
 	
